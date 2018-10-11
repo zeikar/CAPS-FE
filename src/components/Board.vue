9d8879a2f7563fb74b618137d280d4c541ef5d3a
Zeikar <antstp@naver.com>
2018-10-11 16:40:50 +0900

    add header and footer

---
+++ src/components/Board.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="board container">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+            </tr>
+            <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+            </tr>
+            <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Board",
+    props: {}
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+<footer class="py-5 bg-light">
+    <div class="container">
+        <p class="text-center">Copyright &copy; CAPS 2018</p>
+    </div>
+</footer>
+</template>
+
+<script>
+export default {
+    name: "Board",
+    props: {}
+}
+</script>
