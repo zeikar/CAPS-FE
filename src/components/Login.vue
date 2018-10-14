214280c4e7c63fd4c786c47a6aa9fa0fc0ea5c6e
Zeikar <antstp@naver.com>
2018-10-14 17:02:12 +0900

    add eslintrc.js, login prototype

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,26 +1,46 @@
 <template>
 <div class="login container">
-    <form action="/action_page.php">
-        <div class="form-group">
-            <label for="email">Email address:</label>
-            <input type="email" class="form-control" id="email" />
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text">ID</span>
         </div>
-        <div class="form-group">
-            <label for="pwd">Password:</label>
-            <input type="password" class="form-control" id="pwd" />
+        <input type="text" v-model="user_id"
+            class="form-control" id="id" placeholder="아이디를 입력하세요" autocomplete="off" />
+    </div>
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text">PW</span>
         </div>
-        <div class="form-group form-check">
-            <label class="form-check-label">
+        <input type="password" v-model="user_password"
+            class="form-control" id="password" placeholder="비밀번호를 입력하세요" autocomplete="off" />
+    </div>
+    <div class="form-group form-check">
+        <label class="form-check-label">
             <input class="form-check-input" type="checkbox" /> Remember me
         </label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
+    <button @click="onSubmit(user_id, user_password)" class="btn btn-primary btn-block">로그인</button>
 </div>
 </template>
 
 <script>
 export default {
-    name: "Login"
-}
+  name: "Login",
+  data() {
+    return {
+      user_id: "",
+      user_password: ""
+    };
+  },
+  methods: {
+    onSubmit(user_id, user_password) {
+        console.log(user_id + ' ' + user_password);
+        
+      /*this.$store
+        .dispatch("LOGIN", user_id, user_password)
+        .then(() => this.redirect())
+        .catch(({ message }) => (this.msg = message));*/
+    }
+  }
+};
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -6,6 +6,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
+        // 로그인 정보
+        accessToken: null,
+        // 게시판 정보
         boards: []
     },
     getters: {
@@ -16,12 +19,12 @@ export default new Vuex.Store({
     mutations: {
         fetchBoards: (state) => {
             axios.get('http://localhost:3000/boards')
-            .then(response => {
-                state.boards = response.data;
-            })
-            .catch(error => {
-                state.boards = [error];
-            });
+                .then(response => {
+                    state.boards = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     }
 })
