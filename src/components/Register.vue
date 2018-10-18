d91b6ee68e29ab82c17d3d565d2945c1c7bc17d1
Zeikar <antstp@naver.com>
2018-10-18 17:34:49 +0900

    refactoring + add password validation

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -7,36 +7,41 @@
             <form v-on:submit.prevent="onSubmit">
                 <div class="form-group mb-3">
                     <label for="id">아이디</label>
-                    <input type="text" v-model="user_id" :class="isIdValid? 'is-valid' : 'is-invalid'" @keyup="idValidCheck()"
-                        id="id" class="form-control" placeholder="아이디를 입력하세요" autocomplete="off" 
+                    <input type="text" v-model="user_id" :class="isIdValid?'is-valid':'is-invalid'" @keyup="idValidCheck()"
+                        id="id" class="form-control" placeholder="아이디를 입력하세요" autocomplete="off"
                         required autofocus aria-describedby="idHelpBlock" />
                     <div v-show="!isIdValid" id="idHelpBlock" class="invalid-feedback form-text text-danger">
                         <ul>
-                        <li v-for="(message, index) in idValidCheckMessage" v-bind:key="index">
-                            {{ message }}
-                        </li>
+                            <li v-for="(message, index) in idValidCheckMessage" v-bind:key="index">
+                                {{ message }}
+                            </li>
                         </ul>
                     </div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="password">비밀번호</label>
-                    <input type="password" v-model="user_password"
-                        id="password" class="form-control" placeholder="비밀번호를 입력하세요" autocomplete="off" required />
-                    <div class="invalid-feedback">
-                        비밀번호는 8글자 이상, 20글자 이하여야 합니다.
+                    <input type="password" v-model="user_password" :class="isPasswordValid?'is-valid':'is-invalid'" @keyup="passwordValidCheck()"
+                        id="password" class="form-control" placeholder="비밀번호를 입력하세요" autocomplete="off" 
+                        required aria-describedby="passwordHelpBlock" />
+                    <div v-show="!isPasswordValid" id="passwordHelpBlock" class="invalid-feedback form-text text-danger">
+                        <ul>
+                            <li v-for="(message, index) in passwordValidCheckMessage" v-bind:key="index">
+                                {{ message }}
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="password-confirm">비밀번호 확인</label>
-                    <input type="password-confirm" v-model="user_password_confirm"
-                        class="form-control" placeholder="비밀번호를 한 번 더 입력하세요" autocomplete="off" required />
+                    <input type="password" v-model="user_password_confirm"
+                        id="password-confirm" class="form-control" placeholder="비밀번호를 한 번 더 입력하세요" autocomplete="off" required />
                     <div class="invalid-feedback">
                         invalid
                     </div>
                 </div>
                 <div class="form-group mb-3">
                     <label for="name">이름</label>
-                    <input type="text" v-on:input="user_name = $event.target.value"
+                    <input type="text" v-on:input="user_name=$event.target.value"
                         id="name" class="form-control" placeholder="이름을 입력하세요" autocomplete="off" required />
                     <div class="invalid-feedback">
                         invalid
@@ -87,20 +92,24 @@ export default {
 
             // valid check
             idValidCheckMessage: [],
+            passwordValidCheckMessage: [],
 
             // 진행 중
             isProcessing: false
         };
     },
-    computed: {        
+    computed: {
         isIdValid() {
             return this.idValidCheckMessage.length == 0;
+        },
+        isPasswordValid() {
+            return this.passwordValidCheckMessage.length == 0;
         }
     },
     methods: {
         idValidCheck() {
             this.idValidCheckMessage = [];
-            if(this.user_id.length == 0) {
+            if (this.user_id.length == 0) {
                 this.idValidCheckMessage.push('아이디를 입력하세요.');
                 return;
             }
@@ -108,21 +117,34 @@ export default {
             if (this.user_id.length < 4 || this.user_id.length > 20) {
                 this.idValidCheckMessage.push('아이디는 4글자 이상, 20글자 이하여야 합니다.');
             }
-            if(!this.user_id.match(/^[a-z0-9]+$/)) {
+            if (!this.user_id.match(/^[a-z0-9]+$/)) {
                 this.idValidCheckMessage.push('아이디는 영문 소문자, 숫자로만 구성되어야 합니다.');
             }
-             // 존재하는 아이디 체크
+            // 존재하는 아이디 체크
             UserService.checkUserId(this.user_id).then(data => {
-                if(data.message == 'Found') {
+                if (data.message == 'Found') {
                     this.idValidCheckMessage.push('이미 존재하는 아이디입니다.');
                 }
             });
         },
+        passwordValidCheck() {
+            this.passwordValidCheckMessage = [];
+            if (this.user_password.length == 0) {
+                this.passwordValidCheckMessage.push('비밀번호를 입력하세요.');
+                return;
+            }
+
+            if (this.user_password.length < 8 || this.user_password.length > 20) {
+                this.passwordValidCheckMessage.push('비밀번호는 8글자 이상, 20글자 이하여야 합니다.');
+            }
+            if (!this.user_password.match(/^[a-zA-Z0-9]+$/)) {
+                this.passwordValidCheckMessage.push('비밀번호는 영문 대소문자, 숫자로만 구성되어야 합니다.');
+            }
+        },
         onSubmit() {
             this.isProcessing = true;
 
-            this.$store
-                .dispatch('register', {
+            UserService.registerUser({
                     user_id: this.user_id,
                     user_password: this.user_password,
                     user_name: this.user_name,
--- src/service/user.js
+++ src/service/user.js
@@ -2,9 +2,23 @@ import axios from 'axios';
 
 // User에 관련된 axios 액션 정의
 export default {
+    // 아이디 중복 체크
     checkUserId(userId) {
         return new Promise(function (resolve, reject) {
             axios.get('http://localhost:3000/users/check/' + userId)
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    },
+
+    // 회원 가입
+    registerUser(userData) {
+        return new Promise(function (resolve, reject) {
+            axios.post('http://localhost:3000/users/signup', userData)
                 .then(response => {
                     resolve(response.data);
                 })
