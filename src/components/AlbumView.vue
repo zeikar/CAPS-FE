58d314e291644166181823e57dd377c2b3bb2c60
Zeikar <antstp@naver.com>
2018-11-07 17:07:45 +0900

    album view to modal

--- src/components/AlbumView.vue
+++ src/components/AlbumView.vue
@@ -1,48 +1,59 @@
 <template>
-<div class="container" v-if="album && album._id == albumId">
-    <div class="post-heading">
-        <div class="row">
-            <div class="col-md-7 col-sm-12">
-                <h2> {{ album.album_title }} </h2>
-            </div>
-            <div class="col-md-2 col-sm-6">
-                <p> <span class="badge badge-primary">By</span> <a class="colored_name10" :href="'/profile/' + album.user.user_id">{{ album.user.user_name }}</a> </p>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <p> <span class="badge badge-primary">At</span> <a href="#stay_here" data-toggle="tooltip" title="" data-original-title="">{{ album.updated_at }}</a> </p>
+<transition name="modal" v-if="album && album._id == albumId">
+    <div class="modal-mask">
+        <div class="modal-wrapper">
+            <div class="modal-container">
+
+                <div class="modal-header row">
+                    <slot name="header">
+                        <div class="col-md-7 col-sm-12">
+                            <h2> {{ album.album_title }} </h2>
+                        </div>
+                        <div class="col-md-2 col-sm-6">
+                            <p> <span class="badge badge-primary">By</span> <a class="colored_name10" :href="'/profile/' + album.user.user_id">{{ album.user.user_name }}</a> </p>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <p> <span class="badge badge-primary">At</span> <a href="#stay_here" data-toggle="tooltip" title="" data-original-title="">{{ album.updated_at }}</a> </p>
+                        </div>
+                    </slot>
+                </div>
+
+                <div class="modal-body">
+                    <slot name="body">
+                        <div class="row" v-for="photo in album.photos" v-bind:key="photo._id">
+                            <div class="col-sm-12">
+                                <img class="img-fluid" :src="'http://localhost:3000/gallery/photo/' + photo._id">
+                            </div>
+                        </div>
+                    </slot>
+                </div>
+
+                <div class="modal-footer">
+                    <slot name="footer">
+                        <button class="btn btn-danger modal-default-button" @click="closeButtonClicked()">
+                            닫기
+                        </button>
+                    </slot>
+                </div>
             </div>
         </div>
     </div>
-
-    <div class="post-content">
-        <div class="row" v-for="photo in album.photos" v-bind:key="photo._id">
-            <div class="col-sm-12">
-                <img class="img-fluid" :src="'http://localhost:3000/gallery/photo/' + photo._id">
-            </div>
-        </div>
-    </div>
-
-    <div class="post-buttons">
-        <div class="row">
-            <div class="col-sm-2">
-                <a href="../../forum/modify/?id=377" class="btn btn-warning btn-block" role="button">글 수정</a>
-            </div>
-            <div class="col-sm-8"></div>
-            <div class="col-sm-2">
-                <a href="../../forum/delete/?id=377" class="btn btn-danger btn-block" role="button" onclick="return Delete_Confirm()">글 삭제</a>
-            </div>
-        </div>
-    </div>
-
-</div>
+</transition>
 </template>
 
 <script>
 export default {
     name: 'AlbumView',
-    props: ['albumId'],
+    props: {
+        albumId: String
+    },
     mounted() {
         this.$store.dispatch('fetchAlbum', this.albumId);
+    },
+    methods: {
+        closeButtonClicked() {
+            this.$emit('closeModal');
+        }
     },
     computed: {
         album() {
@@ -53,15 +64,69 @@ export default {
 </script>
 
 <style>
-.post-heading {
-    border-bottom: 1px solid #cccccc;
+.modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .9);
+    display: table;
+    transition: opacity .3s ease;
 }
 
-.post-heading h2 {
-    margin-top: 0px;
+.modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
 }
 
-.post-content {
-    border-bottom: 1px solid #cccccc;
+.modal-container {
+    width: 80%;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+    transition: all .3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+    max-height: 80%;
+    overflow-y: scroll;
+}
+
+.modal-header h3 {
+    margin-top: 0;
+    color: #42b983;
+}
+
+.modal-body {
+    margin: 20px 0;
+}
+
+.modal-default-button {
+    float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+    opacity: 0;
+}
+
+.modal-leave-active {
+    opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
 }
 </style>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -6,7 +6,8 @@
                 <img class="card-img-top" :alt="album.album_title" :src="getPhotoUrl(album.photos[0]._id)" >
                 <div class="card-body">
                     <h4 class="card-title">
-                        <router-link :to="'/gallery/view/'+album._id">{{ album.album_title }}</router-link>
+                        <a href="#" data-toggle="modal" @click="albumClick(album._id)"
+                            data-target="#albumModal">{{ album.album_title }}</a>
                     </h4>
                     <p class="card-text">{{ album.created_at }}</p>
                     <a href="#" class="btn btn-primary">좋아요!</a>
@@ -14,27 +15,61 @@
             </div>
         </div>
     </div>
+    <AlbumView v-if="chosenAlbumId!=''&&showModal" v-bind:albumId="chosenAlbumId" v-on:closeModal="closeModal" />
     <router-link :to="'/gallery/upload'" class="btn btn-outline-primary">앨범 생성</router-link>
 </div>
 </template>
 
 <script>
+import AlbumView from './AlbumView.vue';
 import RestAPI from '../constants/RestAPI';
 
 export default {
     name: 'Gallery',
+    data() {
+        return {
+            chosenAlbumId: '',
+            showModal: false
+        };
+    },
     mounted() {
         this.$store.dispatch('fetchGallery');
     },
     methods: {
         getPhotoUrl(photoId) {
             return RestAPI.GET_PHOTO + photoId;
+        },
+        albumClick(albumId) {
+            // 로그인 체크
+            if (!this.$store.getters.isLogined) {
+                this.$notify({
+                    title: '로그인 필요',
+                    text: '앨범을 보시려면 로그인이 필요합니다. 로그인해 주세요.',
+                    type: 'warn'
+                });
+                this.$router.push('/login');
+
+                return;
+            }
+
+            this.chosenAlbumId = albumId;
+
+            this.openModal();
+        },
+        openModal() {
+            this.showModal = true;
+        },
+        closeModal() {
+            this.showModal = false;
         }
     },
     computed: {
         gallery() {
             return this.$store.getters.getGallery;
         }
+    },
+    components: {
+        AlbumView
     }
 };
 </script>
