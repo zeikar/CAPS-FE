3fa34ce091f241ad8f578227ecd92b96d2ee3774
Zeikar <antstp@naver.com>
2018-11-27 15:55:14 +0900

    add file require, format code

--- src/components/AlbumForm.vue
+++ src/components/AlbumForm.vue
@@ -5,7 +5,7 @@
     <form v-on:submit.prevent="onSubmit">
         <InputForm v-bind:initialData="albumTitle" v-on:input="onAlbumTitleChange" name="앨범 제목" />
         <div class="custom-file">
-            <input type="file" accept="image/*"  class="custom-file-input" id="customFileLang" multiple @change="onFileSelected" />
+            <input type="file" required accept="image/*"  class="custom-file-input" id="customFileLang" multiple @change="onFileSelected" />
             <label class="custom-file-label" for="customFileLang">
                 <span v-if="selectedFiles==null">앨범의 사진을 선택하세요.</span>
                 <span v-else>사진 {{selectedFiles.length}}개 선택됨.</span>
@@ -52,64 +52,40 @@ export default {
 
             this.getImagePreviews();
         },
-        getImagePreviews(){
-        /*
-          Iterate over all of the files and generate an image preview for each one.
-        */
-        for( let i = 0; i < this.selectedFiles.length; i++ ){
-          /*
-            Ensure the file is an image file
-          */
-          if ( /\.(jpe?g|png|gif)$/i.test( this.selectedFiles[i].name ) ) {
+        getImagePreviews() {
             /*
-              Create a new FileReader object
+              Iterate over all of the files and generate an image preview for each one.
             */
-            let reader = new FileReader();
+            for (let i = 0; i < this.selectedFiles.length; i++) {
+                /*
+                  Ensure the file is an image file
+                */
+                if (/\.(jpe?g|png|gif)$/i.test(this.selectedFiles[i].name)) {
+                    /*
+                      Create a new FileReader object
+                    */
+                    let reader = new FileReader();
 
-            /*
-              Add an event listener for when the file has been loaded
-              to update the src on the file preview.
-            */
-            reader.addEventListener('load', function(){
-              this.$refs['image'+parseInt( i )][0].src = reader.result;
-            }.bind(this), false);
+                    /*
+                      Add an event listener for when the file has been loaded
+                      to update the src on the file preview.
+                    */
+                    reader.addEventListener('load', function () {
+                        this.$refs['image' + parseInt(i)][0].src = reader.result;
+                    }.bind(this), false);
 
-            /*
-              Read the data for the file in through the reader. When it has
-              been loaded, we listen to the event propagated and set the image
-              src to what was loaded from the reader.
-            */
-            reader.readAsDataURL( this.selectedFiles[i] );
-          }
-        }
-      },
-        // 파일 업로드
-        onUploadButtonClicked() {
-            const formData = new FormData();
-
-            /*
-                Iteate over any file sent over appending the files
-                to the form data.
-            */
-            for (var i = 0; i < this.selectedFiles.length; i++) {
-                let file = this.selectedFiles[i];
-
-                formData.append('image', file);
+                    /*
+                      Read the data for the file in through the reader. When it has
+                      been loaded, we listen to the event propagated and set the image
+                      src to what was loaded from the reader.
+                    */
+                    reader.readAsDataURL(this.selectedFiles[i]);
+                }
             }
-
-            UploadService.uploadImage(formData, uploadEvent => {
-                    this.msg = 'upload : ' + Math.round(uploadEvent.loaded / uploadEvent.total * 100);
-                })
-                .then(response => {
-                    console.log(response);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
         },
         onSubmit() {
 
-            if (!(this.isAlbumTitleValid)) {
+            if (!(this.isAlbumTitleValid) || this.selectedFiles == null || this.selectedFiles.length == 0) {
                 this.$notify({
                     title: '올바르지 않은 입력이 있습니다.',
                     text: '다시 확인 후 입력해주세요.',
