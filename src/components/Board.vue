e0d0155773399c962f366ac223d4e765ac96c0a8
Zeikar <antstp@naver.com>
2018-12-09 14:42:27 +0900

    Add pagination

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -22,7 +22,7 @@
                 <td colspan="5" class="text-center">해당하는 게시글이 없습니다.</td>
             </tr>
             <tr else v-for="(board, index) in boards" v-bind:key="board._id">
-                <td>{{ boards.length - index }}</td>
+                <td>{{ totalCount - (pageQuery - 1) * pageLimit - index }}</td>
                 <td>
                     <a href="javascript:void(0)" @click="boardClick(board._id)">{{ board.board_title }}</a>
                 </td>
@@ -36,9 +36,18 @@
             </tr>
         </tbody>
     </table>
-    <div class="bottom-buttons">
-        <router-link v-if="isLogined()" :to="'/board/write'" class="btn btn-outline-primary">글쓰기</router-link>
-        <div class="input-group inline-input-group mb-3 float-right">
+    <div class="bottom-buttons d-flex justify-content-between align-items-baseline row">
+        <div class="col-sm-3">
+            <router-link v-if="isLogined()" :to="'/board/write'" class="btn btn-outline-primary">글쓰기</router-link>
+        </div>
+        <div class="col-sm-6">
+            <ul class="pagination justify-content-center">
+                <li v-for="page in totalPages" :key="page" :class="page==pageQuery?'active':''" class="page-item">
+                    <a class="page-link" href="javascript:void(0);" @click="pageClick(page)">{{ page }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-sm-3 input-group inline-input-group mb-3 float-right">
             <input type="search" class="form-control" placeholder="검색"
                 @keyup.enter="searchClick()" v-model="searchQuery"/>
             <div class="input-group-append">
@@ -63,7 +72,11 @@ export default {
             isFetching: true,
             searchQuery: '',
             categoryQuery: '',
-            pageQuery: ''
+            pageQuery: 1,
+
+            totalPages: 0,
+            totalCount: 0,
+            pageLimit: 0
         };
     },
     methods: {
@@ -81,14 +94,23 @@ export default {
                 }
             });
         },
-        categoryClick(categoryId) {
+        categoryClick(categoryId) {            
+            this.pageQuery = 1;
             this.categoryQuery = categoryId;
             this.generateQuery();
             this.fetchBoards();
         },
         searchClick() {
+            this.pageQuery = 1;
             this.generateQuery();
             this.fetchBoards();
+        },
+        pageClick(pageNum) {
+            if (pageNum != this.pageQuery) {
+                this.pageQuery = pageNum;
+                this.generateQuery();
+                this.fetchBoards();
+            }
         },
         fetchBoards() {
             this.isFetching = true;
@@ -98,7 +120,10 @@ export default {
                 search: this.$route.query.search,
                 page: this.$route.query.page
             }).then((response) => {
-                this.boards = response.data;
+                this.boards = response.data.result;
+                this.totalPages = response.data.pages;
+                this.totalCount = response.data.count;
+                this.pageLimit = response.data.limit;
                 this.isFetching = false;
             });
         },
